c4f1200a03660201009eb7548f2f56201755f223
David Rollins <drollins@sdcsol.com>
2016-04-02 09:04:05 -0400

    uncomment addPatient

--- Bot Application1/Response.cs
+++ Bot Application1/Response.cs
@@ -15,7 +15,7 @@ namespace Bot_Application1
         public static string GetResponseText(List<Intent> intents, double sentimentScore, int count)
         {
             //return "No matter where you go, there you are...";
-            return intents.OrderBy(x => x.score).First().intent +  " - " + sentimentScore.ToString() + " - " + count;
+            return intents.OrderByDescending(x => x.score).First().intent +  " - " + sentimentScore.ToString() + " - " + count;
         }
     }
 }
